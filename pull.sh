813061a77536c458852c4bb35c208c1ca18f477f
Andrea Censi <andrea@cds.caltech.edu>
2011-09-01 15:39:31 -0700

    add main dir to pull

--- pull.sh
+++ pull.sh
@@ -3,7 +3,7 @@ set -e
 set -x
 ROOT=$PWD
 
-repos="contracts geometry dynamics raytracer vehicles procgraph fly_behaviors"
+repos=". contracts geometry dynamics raytracer vehicles procgraph fly_behaviors"
 for repo in $repos; do
 	cd $ROOT/src/$repo
 	git pull
